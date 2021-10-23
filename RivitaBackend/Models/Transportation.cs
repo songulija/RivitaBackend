05a6f45f7f851ec00221c2831a985f66dbdc7f05
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-10-23 17:31:55 +0300

    Transportation

--- RivitaBackend/Models/Transportation.cs
+++ RivitaBackend/Models/Transportation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,15 @@ namespace RivitaBackend.Models
     public class Transportation
     {
         public int id { get; set; }
+        public int TransportationNumber { get; set; }
+        public string Type { get; set; }
+        public string DepartureStation { get; set; }
+        public string DestinationStation { get; set; }
+        public DateTime MovementStartDate { get; set; }
+        public DateTime MovementEndDate { get; set; }
+        public int NumberOfWagons { get; set; }
+        [ForeignKey(nameof(User))]
+        public int Userid { get; set; }
+        public ApiUser User { get; set; }
     }
 }
