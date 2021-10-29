8aeb3fa308d28363d2f23beb03be1c76fcff9e3f
songulija <65647664+songulija@users.noreply.github.com>
2021-10-29 13:07:02 +0300

    We added IUnitOfWork, UnitOfWork

---
+++ RivitaBackend/IRepository/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using RivitaBackend.IRepository;
+using RivitaBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RivitaBackend.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //basically a register for every variation of Generic Repository
+        IGenericRepository<Transportation> Transportations { get; }
+        IGenericRepository<Wagon> Wagons { get; }
+        // one more operation which is Save. to save all modified record at once
+        Task Save();
+    }
+}
